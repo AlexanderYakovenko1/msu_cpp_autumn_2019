8f795ad5c1e979fce98b9431f72585256ed540d3
Alexander Yakovenko <a.yakovenko@me.com>
2019-11-06 20:12:13 +0300

    Finished hw4

--- 04/bigint.h
+++ 04/bigint.h
@@ -2,36 +2,67 @@
 #define BIGINT_H
 
 #include <iostream>
+#include <iomanip>
 #include <cstdint>
 #include <algorithm>
+
+enum { BASE = 1000000000 };
 
 class BigInt
 {
     bool sign;
-    uint64_t *num;
+    int64_t *num;
     size_t size;
 
+    void shrink_to_fit(BigInt &shr) const
+    {
+        size_t nulls = 0;
+        while (shr.size - 1 - nulls > 0 && !shr.num[shr.size - 1 - nulls]) {
+            ++nulls;
+        }
+        
+        shr.size -= nulls;
+        int64_t *buf = new int64_t[shr.size];
+        for (size_t i = 0; i < shr.size; ++i) {
+            buf[i] = shr.num[i];
+        }
+
+        delete[] shr.num;
+        shr.num = buf;
+
+        // fix -0
+        if (shr.size == 1 && shr.num[0] == 0) {
+            shr.sign = false;
+        }
+    }
+
 public:
-    BigInt(int base=0) : num(new uint64_t[1]), size(1)
+    BigInt(int base=0) : num(new int64_t[2]), size(2)
     {
         if (base >= 0) {
             sign = false;
-            num[0] = base;
+            num[0] = base % BASE;
+            num[1] = base / BASE;
         } else {
             sign = true;
-            num[0] = -base;
+            num[0] = -(base % BASE);
+            num[1] = -(base / BASE);
         }
+        shrink_to_fit(*this);
     }
 
     BigInt(int base, size_t size) : size(size)
     {
         sign = false;
-        num = new uint64_t[size];
+        num = new int64_t[size];
+        for (size_t i = 0; i < size; ++i) {
+            num[i] = 0;
+        }
     }
 
     BigInt(const BigInt &copy) : sign(copy.sign), size(copy.size)
     {
-        num = new uint64_t[size];
+        num = new int64_t[size];
         for (size_t i = 0; i < size; ++i) {
             num[i] = copy.num[i];
         }
@@ -47,35 +78,100 @@ public:
         delete[] num;
     }
 
+    bool operator ==(const BigInt &cmp) const
+    {
+        if (sign == cmp.sign && size == cmp.size) {
+            for (size_t i = 0; i < size; ++i) {
+                if (num[i] != cmp.num[i]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool operator !=(const BigInt &cmp) const
+    {
+        return !(*this == cmp);
+    }
+
+    bool operator <(const BigInt &cmp) const
+    {
+        if (sign && !cmp.sign) {
+            return true;
+        } else if (!sign && cmp.sign) {
+            return false;
+        }
+        if (size < cmp.size) {
+            return !sign;
+        } else if (size > cmp.size) {
+            return sign;
+        }
+        for (size_t i = 0; i < size; ++i) {
+            int64_t dif = num[size - i - 1] - cmp.num[size - i - 1];
+            if (dif < 0) {
+                return !sign;
+            } else if (dif > 0) {
+                return sign;
+            }
+        }
+        return false;
+    }
+
+    bool operator >(const BigInt &cmp) const
+    {
+        return !(*this == cmp || *this < cmp);
+    }
+
+    bool operator >=(const BigInt &cmp) const
+    {
+        return (*this == cmp || *this > cmp);
+    }
+
+    bool operator <=(const BigInt &cmp) const
+    {
+        return (*this == cmp || *this < cmp);
+    }
+
+    BigInt abs() const
+    {
+        BigInt res(*this);
+        res.sign = false;
+        return res;
+    }
+
     BigInt operator +(const BigInt &add) const
     {
         size_t max_size = std::max(size, add.size);
         size_t min_size = std::min(size, add.size);
 
         BigInt res(0, max_size + 1);
-        uint64_t carryover = 0;
+        int64_t carryover = 0;
         
         if (sign == add.sign) {
             res.sign = sign;
             for (size_t i = 0; i < min_size; ++i) {
                 res.num[i] = num[i] + add.num[i] + carryover;
-                carryover = res.num[i] >> 4;
-                res.num[i] &= 0xFFFFFFFF;
+                carryover = res.num[i] / BASE;
+                res.num[i] %= BASE;
             }
 
             if (max_size == size) {
                 for (size_t i = min_size; i < max_size; ++i) {
                     res.num[i] = num[i] + carryover;
-                    carryover = res.num[i] >> 4;
-                    res.num[i] &= 0xFFFFFFFF;
+                    carryover = res.num[i] / BASE;
+                    res.num[i] %= BASE;
                 }
             } else {
                 for (size_t i = min_size; i < max_size; ++i) {
                     res.num[i] = add.num[i] + carryover;
-                    carryover = res.num[i] >> 4;
-                    res.num[i] &= 0xFFFFFFFF;
+                    carryover = res.num[i] / BASE;
+                    res.num[i] %= BASE;
                 }
             }
+            shrink_to_fit(res);
             return res;
         } else {
             return *this - (-add);
@@ -94,30 +190,38 @@ public:
         size_t max_size = std::max(size, sub.size);
         size_t min_size = std::min(size, sub.size);
 
+        if (*this < sub && !(sign && sub.sign)) {
+            return -(sub - *this);
+        }
+
         BigInt res(0, max_size);
-        uint64_t carryover = 0;
+        int64_t carryover = 0;
 
         if (sign == sub.sign) {
             res.sign = sign;
             for (size_t i = 0; i < min_size; ++i) {
                 res.num[i] = num[i] - sub.num[i] - carryover;
-                carryover = res.num[i] >> 4;
-                res.num[i] &= 0xFFFFFFFF;
+                carryover = res.num[i] < 0;
+                res.num[i] += BASE;
+                res.num[i] %= BASE;
             }
 
             if (max_size == size) {
                 for (size_t i = min_size; i < max_size; ++i) {
                     res.num[i] = num[i] - carryover;
-                    carryover = res.num[i] >> 4;
-                    res.num[i] &= 0xFFFFFFFF;
+                    carryover = res.num[i] < 0;
+                    res.num[i] += BASE;
+                    res.num[i] %= BASE;
                 }
             } else {
                 for (size_t i = min_size; i < max_size; ++i) {
                     res.num[i] = sub.num[i] - carryover;
-                    carryover = res.num[i] >> 4;
-                    res.num[i] &= 0xFFFFFFFF;
+                    carryover = res.num[i] < 0;
+                    res.num[i] += BASE;
+                    res.num[i] %= BASE;
                 }
             }
+            shrink_to_fit(res);
             return res;
         } else {
             return *this + (-sub);
@@ -128,11 +232,9 @@ public:
         if (out.sign) {
             os << '-';
         }
-        if (out.size == 0) {
-            os << '0';
-        }
-        for (ssize_t i = out.size - 1; i >= 0; --i) {
-            os << out.num[i];
+        os << out.num[out.size - 1];
+        for (ssize_t i = out.size - 2; i >= 0; --i) {
+            os << std::setw(9) << std::setfill('0') << out.num[i];
         }
         return os;
     }
--- 04/check.cpp
+++ 04/check.cpp
@@ -6,15 +6,85 @@
 
 int main(int argc, char const *argv[])
 {
-    // if (argc != 2) {
-    //     std::cerr << "Usage: " << argv[0] << "test_num" << std::endl;
-    //     return 1;
-    // }
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " test_num" << std::endl;
+        return 1;
+    }
 
-    BigInt a = -2000000000;
-    BigInt b = -2000000000;
+    BigInt a(10);
+    BigInt aa(10);
+    BigInt max_int(2147483647);
+    BigInt min_int(-2147483648);
 
-    std::cout << a - b << std::endl;
-
+    switch (std::stoi(argv[1]))
+    {
+        case 1:
+        {
+            assert(a == a);
+            assert(a == aa);
+            break;
+        }
+        case 2:
+        {
+            assert((a + 1) > a);
+            assert(a - 1 < a);
+            break;
+        }
+        case 3:
+        {
+            assert(max_int + max_int == -min_int + -min_int - 2);
+            break;
+        }
+        case 4:
+        {
+            std::cout << max_int + min_int << std::endl;
+            break;
+        }
+        case 5:
+        {
+            assert(a <= max_int);
+            break;
+        }
+        case 6:
+        {
+            assert(aa + a == 20);
+            break;
+        }
+        case 7:
+        {
+            assert(-a == -aa);
+            break;
+        }
+        case 8:
+        {
+            assert(!(a != a));
+            break;
+        }
+        case 9:
+        {
+            std::cout << a - a << std::endl;
+            break;
+        }
+        case 10:
+        {
+            std::cout << max_int - max_int << std::endl;
+            break;
+        }
+        case 11:
+        {
+            std::cout << min_int - min_int << std::endl;
+            break;
+        }
+        case 12:
+        {
+            std::cout << min_int - min_int - min_int << std::endl;
+            break;
+        }
+        case 13:
+        {
+            std::cout << max_int + max_int + max_int << std::endl;
+            break;
+        }
+    }
     return 0;
 }

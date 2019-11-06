d01a22f37df36347c7bb61e97db8f2b7329454c5
AlexanderYakovenko1 <a.yakovenko@me.com>
2019-11-06 15:48:45 +0300

    Contued doing hw4

--- 04/bigint.h
+++ 04/bigint.h
@@ -1,6 +1,7 @@
 #ifndef BIGINT_H
 #define BIGINT_H
 
+#include <iostream>
 #include <cstdint>
 #include <algorithm>
 
@@ -9,10 +10,9 @@ class BigInt
     bool sign;
     uint64_t *num;
     size_t size;
-    size_t capacity;
 
 public:
-    BigInt(int base=0) : num(new uint64_t[1]), size(1), capacity(1)
+    BigInt(int base=0) : num(new uint64_t[1]), size(1)
     {
         if (base >= 0) {
             sign = false;
@@ -22,10 +22,24 @@ public:
             num[0] = -base;
         }
     }
-    BigInt(int base=0, size_t size) : size(size), capacity(size)
+
+    BigInt(int base, size_t size) : size(size)
     {
         sign = false;
         num = new uint64_t[size];
+    }
+
+    BigInt(const BigInt &copy) : sign(copy.sign), size(copy.size)
+    {
+        num = new uint64_t[size];
+        for (size_t i = 0; i < size; ++i) {
+            num[i] = copy.num[i];
+        }
+    }
+
+    BigInt(BigInt &&mov) : sign(mov.sign), size(mov.size), num(mov.num)
+    {
+        mov.num = nullptr;
     }
 
     ~BigInt()
@@ -38,7 +52,7 @@ public:
         size_t max_size = std::max(size, add.size);
         size_t min_size = std::min(size, add.size);
 
-        res = BigInt(0, max_size + 1);
+        BigInt res(0, max_size + 1);
         uint64_t carryover = 0;
         
         if (sign == add.sign) {
@@ -62,9 +76,65 @@ public:
                     res.num[i] &= 0xFFFFFFFF;
                 }
             }
+            return res;
         } else {
-            
+            return *this - (-add);
         }
+    }
+
+    BigInt operator -() const
+    {
+        BigInt res(*this);
+        res.sign = !res.sign;
+        return res;
+    }
+
+    BigInt operator -(const BigInt &sub) const
+    {
+        size_t max_size = std::max(size, sub.size);
+        size_t min_size = std::min(size, sub.size);
+
+        BigInt res(0, max_size);
+        uint64_t carryover = 0;
+
+        if (sign == sub.sign) {
+            res.sign = sign;
+            for (size_t i = 0; i < min_size; ++i) {
+                res.num[i] = num[i] - sub.num[i] - carryover;
+                carryover = res.num[i] >> 4;
+                res.num[i] &= 0xFFFFFFFF;
+            }
+
+            if (max_size == size) {
+                for (size_t i = min_size; i < max_size; ++i) {
+                    res.num[i] = num[i] - carryover;
+                    carryover = res.num[i] >> 4;
+                    res.num[i] &= 0xFFFFFFFF;
+                }
+            } else {
+                for (size_t i = min_size; i < max_size; ++i) {
+                    res.num[i] = sub.num[i] - carryover;
+                    carryover = res.num[i] >> 4;
+                    res.num[i] &= 0xFFFFFFFF;
+                }
+            }
+            return res;
+        } else {
+            return *this + (-sub);
+        }
+    }
+
+    friend std::ostream& operator <<(std::ostream& os, const BigInt &out) {
+        if (out.sign) {
+            os << '-';
+        }
+        if (out.size == 0) {
+            os << '0';
+        }
+        for (ssize_t i = out.size - 1; i >= 0; --i) {
+            os << out.num[i];
+        }
+        return os;
     }
     
 };
--- 04/check.cpp
+++ 04/check.cpp
@@ -2,88 +2,19 @@
 #include <sstream>
 #include <cassert>
 
-#include "matrix.h"
+#include "bigint.h"
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << "test_num" << std::endl;
-        return 1;
-    }
+    // if (argc != 2) {
+    //     std::cerr << "Usage: " << argv[0] << "test_num" << std::endl;
+    //     return 1;
+    // }
 
-    const size_t rows = 5;
-    const size_t cols = 3;
-    Matrix m(rows, cols);
-    const Matrix m1(rows, cols);
+    BigInt a = -2000000000;
+    BigInt b = -2000000000;
 
-    switch (std::stoi(argv[1]))
-    {
-        case 1:
-        {
-            assert(m.getRows() == 5);
-            assert(m.getColumns() == 3);
-            break;
-        }
-        case 2:
-        {
-            m[1][2] = 5;
-            break;
-        }
-        case 3:
-        {
-            double x = m[4][1];
-            break;
-        }
-        case 4:
-        {
-            m *= 3;
-            break;
-        }
-        case 5:
-        {
-            assert(m == m);
-            break;
-        }
-        case 6:
-        {
-            assert(m1 == m1);
-            break;
-        }
-        case 7:
-        {
-            m[0][0] = 10;
-            assert(m1 != m);
-            assert(m != m1);
-            break;
-        }
-        case 8:
-        {
-            assert(!(m != m));
-            break;
-        }
-        case 9:
-        {
-            assert(!(m1 != m1));
-            break;
-        }
-        case 10:
-        {
-            try {
-                m[10][0] = 2;
-            } catch (std::exception &exc) {
-                std::cout << exc.what() << std::endl;
-                return 1;
-            }
-        }
-        case 11:
-        {
-            try {
-                m[0][10] = 2;
-            } catch (std::exception &exc) {
-                std::cout << exc.what() << std::endl;
-                return 1;
-            }
-        }
-    }
+    std::cout << a - b << std::endl;
+
     return 0;
 }
